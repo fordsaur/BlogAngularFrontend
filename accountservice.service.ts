8ae19d56b89fccdd9ab449336c6a9e62dece9d5b
Saurabh Kolarkar <46285464+fordsaur@users.noreply.github.com>
2024-02-27 22:17:35 +0530

    Add files via upload

---
+++ accountservice.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { provideHttpClient } from "@angular/common/http";
+import { Accountinfo } from './accountinfo';
+import { Userloginfo } from './userloginfo';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AccountService {
+  url = 'https://blognodebackend-3.onrender.com/';
+  constructor(private http: HttpClient) {}
+
+
+
+  createaccount(accinfo: Accountinfo): any{
+    return this.http.post(this.url+"api/register",accinfo);
+    console.log("service",accinfo)
+  }
+
+  userlogin(logininfo:Userloginfo):Observable<Userloginfo>{
+   return this.http.post<Userloginfo>(this.url+'api/login',logininfo)
+  }
+}
